6b528974ad820ed39933a61247e25ccd11fabc18
Evelyn Paplauskas <evelyn.paplauskas@outlook.com>
2024-02-27 16:48:25 +1100

    Completed step 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HeroesComponent, HeroDetailComponent],
+  declarations: [AppComponent, MessagesComponent, HeroesComponent],
+  imports: [BrowserModule, FormsModule, HeroDetailComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,34 @@
-import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
-import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
-  standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  imports: [HeroDetailComponent, NgFor],
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  selectedHero?: Hero;
 
-  selectedHero!: Hero;
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
