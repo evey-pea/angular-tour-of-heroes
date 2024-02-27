c1e46f2d140ab8b9373e00f49bf53783cd0a951c
Evelyn Paplauskas <evelyn.paplauskas@outlook.com>
2024-02-27 16:32:17 +1100

    Completed step 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HeroesComponent],
+  imports: [BrowserModule, FormsModule, HeroesComponent, HeroDetailComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
